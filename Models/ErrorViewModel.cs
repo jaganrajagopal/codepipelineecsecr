5cdc7e3534489b7d6aeddd5153f328acb39a3909
Jagan Rajagopal <jaganrajagopal@outlook.com>
2023-12-21 10:43:55 +0530

    adding for dotnetproject for ecs and ecr

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace awscodepipeline.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
